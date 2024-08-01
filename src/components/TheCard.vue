d32d8e1e54200a9076e9f08996b7b0477d9ea645
kiichi <kiichi.kawashima55@gmail.com>
2024-08-01 19:34:32 +0900

    画像が表示されない問題を対応してみた（途中）

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,23 +1,23 @@
-<script setup lang='ts'>
+<script setup lang="ts">
+import { computed } from 'vue'
+
 interface Props {
-    picture:string
-    title:string
-    text:string
+  picture: string
+  title: string
+  text: string
 }
 
 const props = defineProps<Props>()
 </script>
 <template>
-    <div class="card card-compact bg-base-100 w-80 shadow-xl card-bordered static">
-      <figure>
-        <img
-          :src="props.picture"
-          alt="dishes"
-        />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">{{ props.title }}</h2>
-        <p>{{ props.text }}</p>
-      </div>
+  <div class="card card-compact bg-base-100 w-80 shadow-xl card-bordered static">
+    <figure>
+      <img :src="props.picture" alt="dishes" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ props.title }}</h2>
+      <p>{{ props.text }}</p>
+      {{ props.picture }}
     </div>
+  </div>
 </template>
